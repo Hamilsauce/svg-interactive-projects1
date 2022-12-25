9e705469c252a81f18eeb116300ae8029458f3b6
jake@seahag.onmicrosoft.com <jake@seahag.onmicrosoft.com>
2022-12-25 16:54:32 -0600

    nk

---
+++ plot-points/entities/crosshair.js
@@ -0,0 +1,85 @@
+import { toTrackPoint } from '/svg-trackpad/lib.js';
+
+const { forkJoin, Observable, iif, BehaviorSubject, AsyncSubject, Subject, interval, of , fromEvent, merge, empty, delay, from } = rxjs;
+const { flatMap, reduce, groupBy, toArray, mergeMap, switchMap, scan, map, tap, filter } = rxjs.operators;
+const { fromFetch } = rxjs.fetch;
+
+export class Crosshair {
+  #inputSubscription
+
+  constructor(initialPoint = { x: 0, y: 0 }, el) {
+    this.name = 'crosshair';
+    this.basePoint = { x: 0, y: 0 }
+    this.size = 11;
+    this.d = '';
+
+    this.state = new BehaviorSubject(initialPoint)
+      .pipe(
+        // filter((p)=> p.x && p.y),
+        scan((prevPoint, newPoint) => {
+          console.warn('scan newPoint', newPoint)
+          // const point = { ...prevPoint, ...newPoint }
+          return {
+            // x: newPoint.x + (prevPoint.x - newPoint.x),
+            // y: newPoint.y + (prevPoint.y - newPoint.y),
+            x: prevPoint.x + (newPoint.x - prevPoint.x),
+            y: prevPoint.y + (newPoint.y - prevPoint.y),
+            // ...newPoint
+          }
+        }, this.basePoint),
+        // tap(x => console.warn('newPoint', x)),
+        map(this.update.bind(this)),
+      )
+  }
+
+  connectInput(controlStream$) {
+    this.$inputSubscription = controlStream$
+      .pipe().subscribe(this.state);
+
+    return this.$inputSubscription;
+  }
+
+  watch() {
+    return this.state.asObservable();
+  }
+
+  get bounds() {
+    return {
+      top: (this.basePoint.y - (this.size / 2)),
+      bottom: (this.basePoint.y + (this.size / 2)),
+      left: (this.basePoint.x - (this.size / 2)),
+      right: (this.basePoint.x + (this.size / 2)),
+    }
+  }
+
+  update(point = { x: 0, y: 0 }) {
+    this.basePoint = point;
+
+    const { top, bottom, left, right } = this.bounds;
+    const { x, y } = point;
+
+    let d = `
+     M ${left},${y}
+       -50,${y}
+     M ${right},${y} 
+       50,${y} 
+     M ${x},${top} 
+       ${x},-50
+     M ${x},${bottom} 
+       ${x},50
+     M ${left},${top}
+       ${right},${top}
+       ${right},${bottom}
+       ${left},${bottom}
+    z`.trim();
+
+    this.d = d;
+
+    return {
+      ...this.bounds,
+      name: this.name,
+      point,
+      d,
+    }
+  }
+}
---
+++ plot-points/entities/pawn.js
@@ -0,0 +1,75 @@
+import { toTrackPoint } from '/svg-trackpad/lib.js';
+
+const { forkJoin, Observable, iif, BehaviorSubject, AsyncSubject, Subject, interval, of , fromEvent, merge, empty, delay, from } = rxjs;
+const { distinctUntilChanged, flatMap, reduce, groupBy, toArray, mergeMap, switchMap, scan, map, tap, filter } = rxjs.operators;
+const { fromFetch } = rxjs.fetch;
+
+export class Pawn {
+  #inputSubscription;
+  #isTargeted;
+  currentInput;
+
+  constructor(point = { x: 0, y: 0 }, el) {
+    this.name = 'pawn';
+
+    this.basePoint = { x: -25, y: -25 };
+
+    this.size = 5;
+
+    this.position$ = new BehaviorSubject(this.basePoint)
+      .pipe(
+        tap(this.update.bind(this)),
+        scan((prevPoint, { x, y }) => ({
+          ...prevPoint,
+          ...{ cx: x, cy: y },
+          ...this.bounds,
+          r: this.size,
+        }), {
+          cx: this.basePoint.x,
+          cy: this.basePoint.y,
+          r: this.size,
+          name: this.name,
+        }),
+      )
+  }
+
+  get isTargeted() { return this.#isTargeted }
+
+  set isTargeted(newValue) { this.#isTargeted = newValue };
+
+  connectInput(controlStream$) {
+    this.$inputSubscription = controlStream$
+      .pipe().subscribe(this.position$)
+
+    return this.$inputSubscription
+  }
+
+  disconnectInput() {
+    this.$inputSubscription.unsubscribe();
+    return this.$inputSubscription;
+  }
+
+  watch() {
+    return this.position$.asObservable();
+  }
+
+  get bounds() {
+    return {
+      top: (this.basePoint.y - (this.size / 2)),
+      bottom: (this.basePoint.y + (this.size / 2)),
+      left: (this.basePoint.x - (this.size / 2)),
+      right: (this.basePoint.x + (this.size / 2)),
+    }
+  }
+
+  update(point = { x: 0, y: 0 }) {
+    this.basePoint = point
+
+    return {
+      ...this.bounds,
+      name: this.name,
+      r: this.size,
+      point
+    }
+  }
+}
