a1166a1c4feabacbd64dfbeb99783468d16e7e75
hamilsauce <jacob.w.hamilton@outlook.com>
2025-04-09 15:04:09 -0500

    update svgpad.js

--- svg-trackpad/svgpad.js
+++ svg-trackpad/svgpad.js
@@ -2,8 +2,8 @@ import {
   toTrackPoint,
   toScenePoint,
   addVectors
-} from '../svg-trackpad/lib.js';
-import { Scene } from '../svg-trackpad/entities/scene.js'
+} from '/svg-trackpad/lib.js';
+import { Scene } from '/svg-trackpad/entities/scene.js'
 
 // const { forkJoin, Observable, iif, BehaviorSubject, AsyncSubject, Subject, interval, of, fromEvent, merge, empty, delay, from } = rxjs;
 // const { flatMap, reduce, groupBy, toArray, mergeMap, switchMap, scan, map, tap, filter } = rxjs.operators;
